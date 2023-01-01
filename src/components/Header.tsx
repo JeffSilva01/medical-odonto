1b90521ce28ddec43feb482d0b83f81b31e86ce9
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-01-01 18:46:52 -0300

    add images

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+export function Header() {
+  return (
+    <header>
+      <img src="" alt="Logo Medical Odonto" />
+      <nav>
+        <a href="#">AGENDAMENTO</a>
+        <a href="#">NOSSA HISTÓRIA</a>
+        <a href="#">FOTOS</a>
+        <a href="#">crc</a>
+        <a href="#">uNIDADES</a>
+      </nav>
+    </header>
+  );
+}
